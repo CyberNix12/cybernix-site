28e22dbc9f1652d5d46039e44eb93b769c6a5678
CyberNix12 <cybernixwebkraft@gmail.com>
2025-02-18 00:17:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("darkModeToggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
+  });
+  
